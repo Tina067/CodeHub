62e055dc93a62faaa62d3f7007ed25895498fd04
tina067 <tinamurmu222@gmail.com>
2024-11-07 12:03:17 +0530

    3rd commit 07-11-2024

--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './ProgressCard.css';
 
 const ProgressCard = () => {
+    const boxes = Array.from({length: 24})
   return (
     <div className='OneCard'>
       <div className='topic'></div>
-      <div className='ProCard'></div>
+      <div className='ProCard'>
+        <div className="box-container">
+            {boxes.map((_, index) => (
+                <div key={index} className="small-box"> </div>
+            ))}
+        </div>
+      </div>
     </div>
    
   )
 }
 
 export default ProgressCard
+
